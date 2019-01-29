403943606c7c212bec845a51de277595566e2bbe
Aaron Gullickson <aarong@uoregon.edu>
2019-01-29 09:21:05 -0800

    added a script for loading all example datasets

---
+++ example_datasets/load_all_data.R
@@ -0,0 +1,10 @@
+#load all the example datasets
+
+load("example_datasets/add_health/addhealth.RData")
+load("example_datasets/crimes/crimes.RData")
+load("example_datasets/movies/movies.RData")
+load("example_datasets/nlsy/nlsydropout.RData")
+load("example_datasets/politics/politics.RData")
+load("example_datasets/sex/sex.RData")
+load("example_datasets/titanic/titanic.RData")
+load("example_datasets/wbreport/wbreport.RData")
