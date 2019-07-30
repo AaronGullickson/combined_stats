523d5163f11806ee93de541696efa16dca127ce3
Aaron Gullickson <aarong@uoregon.edu>
2019-07-30 09:41:40 -0700

    finished xaringan version of stat inference slides

--- slides/publish_slides.sh
+++ slides/publish_slides.sh
@@ -1,6 +1,8 @@
 Rscript -e 'rmarkdown::render("module1_slides_understanding_data.Rmd")'
 Rscript -e 'rmarkdown::render("module2_slides_distribution_variable.Rmd")'
 Rscript -e 'rmarkdown::render("module3_slides_measuring_association.Rmd")'
+Rscript -e 'rmarkdown::render("module4_slides_statistical_inference.Rmd")'
+
 
 #copy over stuff, but don't overwrite
 cp -R -n -p images ~/Professional/blog/stat_slides/
@@ -16,6 +18,9 @@ cp -R module2_slides_distribution_variable_files ~/Professional/blog/stat_slides
 
 cp module3_slides_measuring_association.html ~/Professional/blog/stat_slides/
 cp -R module3_slides_measuring_association_files ~/Professional/blog/stat_slides/
+
+cp module4_slides_statistical_inference.html ~/Professional/blog/stat_slides/
+cp -R module4_slides_statistical_inference_files ~/Professional/blog/stat_slides/
 
 
 #remove all created files
