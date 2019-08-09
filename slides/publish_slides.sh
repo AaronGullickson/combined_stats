3539d8ab62bc6b1d83b997a9b7870837c4a220fe
Aaron Gullickson <aarong@uoregon.edu>
2019-08-09 14:26:51 -0700

    module 5 lecture slides xaringan style

--- slides/publish_slides.sh
+++ slides/publish_slides.sh
@@ -2,6 +2,8 @@ Rscript -e 'rmarkdown::render("module1_slides_understanding_data.Rmd")'
 Rscript -e 'rmarkdown::render("module2_slides_distribution_variable.Rmd")'
 Rscript -e 'rmarkdown::render("module3_slides_measuring_association.Rmd")'
 Rscript -e 'rmarkdown::render("module4_slides_statistical_inference.Rmd")'
+Rscript -e 'rmarkdown::render("module5_slides_building_models.Rmd")'
+
 
 
 #copy over stuff, but don't overwrite
@@ -22,6 +24,8 @@ cp -R module3_slides_measuring_association_files ~/Professional/blog/stat_slides
 cp module4_slides_statistical_inference.html ~/Professional/blog/stat_slides/
 cp -R module4_slides_statistical_inference_files ~/Professional/blog/stat_slides/
 
+cp module5_slides_building_models.html ~/Professional/blog/stat_slides/
+cp -R module5_slides_building_models_files ~/Professional/blog/stat_slides/
 
 #remove all created files
 rm *.html
